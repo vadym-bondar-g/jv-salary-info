442466144dee556f98001f3953ada37906ffe07a
vadym <evansfalter@gmail.com>
2026-08-21 00:55:51 +0200

    few changes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.List;
 
 public class SalaryInfo {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -15,7 +16,6 @@ public class SalaryInfo {
         StringBuilder totalReport = new StringBuilder();
         String[] dataArray;
         Map<String, Integer> salaries = new LinkedHashMap<>();
-
         for (int i = 0; i < data.length; i++) {
             dataArray = data[i].split(" ");
             int salary = 0;
@@ -27,17 +27,16 @@ public class SalaryInfo {
             }else  {
                 salaries.merge(name, 0, Integer::sum);
             }
-
         }
         totalReport.append(REPORT).append(dateFrom).append(" - ").append(dateTo).append('\n');
         List<Map.Entry<String, Integer>> sortedSalaries = salaries.entrySet().stream().toList();
         for (int i = 0; i < sortedSalaries.size(); i++) {
-            totalReport.append(sortedSalaries.get(i).getKey()).append(" - ").append(sortedSalaries.get(i).getValue());
+            totalReport.append(sortedSalaries.get(i).getKey()).append(" - ")
+                    .append(sortedSalaries.get(i).getValue());
             if (sortedSalaries.size() > i + 1) {
                 totalReport.append('\n');
             }
         }
-
         return totalReport.toString();
     }
 }
