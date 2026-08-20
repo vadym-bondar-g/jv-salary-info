18cfc279ddf276f95e9f14b774f284655daad9ca
vadym <evansfalter@gmail.com>
2026-08-21 00:51:26 +0200

    feat: implement salaryInfo class

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,21 +2,42 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String REPORT = "Report for period ";
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate from = LocalDate.parse(dateFrom, formatter);
         LocalDate to = LocalDate.parse(dateTo, formatter);
-        for (String dataName : data) {
-            dataName = dataName.trim();
+        LocalDate localDate;
+        StringBuilder totalReport = new StringBuilder();
+        String[] dataArray;
+        Map<String, Integer> salaries = new LinkedHashMap<>();
 
+        for (int i = 0; i < data.length; i++) {
+            dataArray = data[i].split(" ");
+            int salary = 0;
+            salary = Integer.parseInt(dataArray[2]) * Integer.parseInt(dataArray[3]);
+            String name = dataArray[1];
+            localDate = LocalDate.parse(dataArray[0], formatter);
+            if (!localDate.isBefore(from) && !localDate.isAfter(to)) {
+                salaries.merge(name, salary, Integer::sum);
+            }else  {
+                salaries.merge(name, 0, Integer::sum);
+            }
 
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append("Report for period "+dateFrom+" - "+dateTo+"\n");
-        sb.append("Total salary for "+data[0]+"\n");
+        totalReport.append(REPORT).append(dateFrom).append(" - ").append(dateTo).append('\n');
+        List<Map.Entry<String, Integer>> sortedSalaries = salaries.entrySet().stream().toList();
+        for (int i = 0; i < sortedSalaries.size(); i++) {
+            totalReport.append(sortedSalaries.get(i).getKey()).append(" - ").append(sortedSalaries.get(i).getValue());
+            if (sortedSalaries.size() > i + 1) {
+                totalReport.append('\n');
+            }
+        }
 
-        return sb.toString();
+        return totalReport.toString();
     }
 }
